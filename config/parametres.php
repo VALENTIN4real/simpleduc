61605a127cae64b9dd5736e2100728bfb7d85f1b
Imrane <imranekent@gmail.com>
2021-09-28 17:22:28 +0200

    db working

---
+++ config/parametres.php
@@ -0,0 +1,6 @@
+<?php  
+    $config['serveur']='localhost';  
+    $config['login'] = 'login4223';  
+    $config['mdp'] ='lDirVLutHrFQtUP';  
+    $config['bd'] = 'simpleducDB';
+?>
--- config/routes.php
+++ config/routes.php
@@ -1,22 +1,26 @@
 <?php
-function getPage(){
+function getPage($db){
 
     $lesPages['accueil'] = "accueilControleur";
     $lesPages['connexion'] = "connexionControleur";
+    $lesPages['maintenance'] = "maintenanceControleur";
     
-    if (isset($_GET['page'])){
-        $page = $_GET['page'];
-    }
-    else{
-        $page = 'accueil';
-    }
-    if (isset($lesPages[$page])){
-        $contenu = $lesPages[$page];
-    }
-    else{
-        $contenu = $lesPages['accueil'];
-    }
-    
+    if($db!=null){
+        if (isset($_GET['page'])){
+            $page = $_GET['page'];
+        }
+        else{
+            $page = 'accueil';
+        }
+        if (isset($lesPages[$page])){
+            $contenu = $lesPages[$page];
+        }
+        else{
+            $contenu = $lesPages['accueil'];
+        }
+    }else{
+        $contenu = $lesPages['maintenance'];
+    } 
     return $contenu;
 }
 ?>
