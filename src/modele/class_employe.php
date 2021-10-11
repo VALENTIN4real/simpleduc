3f70a9134fed94ded5f9c854c1a72b7ed635376e
Imrane <imranekent@gmail.com>
2021-10-11 19:30:07 +0200

    profil info complète

--- src/modele/class_employe.php
+++ src/modele/class_employe.php
@@ -11,7 +11,7 @@ class Employe{
         $this->insert = $this->db->prepare("insert  into  Employe(email,  mdp,  nom,  prenom,  idRole)values (:email, :mdp, :nom, :prenom, :role)");
         $this->connect = $this->db->prepare("select   C.email,   E.idRole,   C.mdp   from   Employe E, Compte C where E.idCompte = C.id and C.email=:email");
         $this->select = $db->prepare("select E.id, email, nom, prenom, idRole, r.libelle as libellerole from Employe E, role r where E.idRole = r.id order by email");
-        $this->selectByAccount = $this->db->prepare("SELECT E.id, nom, prenom, email, idRole, idCompte FROM Employe E, Compte C WHERE C.email = :email");
+        $this->selectByAccount = $this->db->prepare("SELECT E.id, nom, prenom, adresse, adresseBis, region,codePostal, numTel , email, idRole,r.libelle, idCompte FROM Employe E, Compte C, Role r WHERE C.email = :email AND idRole = r.id");
     }
 
     public function insert($email, $mdp, $role, $nom, $prenom){ // Étape 3         
