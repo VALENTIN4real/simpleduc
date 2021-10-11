ad7db642e06aa7282cb5083f9573dcc2d2137639
Imrane <imranekent@gmail.com>
2021-10-11 16:06:00 +0200

    profile on working

--- src/controleur/accueilControleur.php
+++ src/controleur/accueilControleur.php
@@ -1,5 +1,9 @@
 <?php
-function accueilControleur($twig){
-    echo $twig->render('accueil.html.twig', array());
+function accueilControleur($twig,$db){
+    $form = array();
+    if(isset($_GET['item'])){
+        $form['item'] = $_GET['item'];
+    }
+    echo $twig->render('accueil.html.twig', array('form' => $form));
 }
 ?>
--- src/modele/class_employe.php
+++ src/modele/class_employe.php
@@ -4,14 +4,16 @@ class Employe{
     private $insert; 
     private $connect;
     private $select;
+    private $selectByAccount;
 
 
     public function __construct($db){        
         $this->db = $db;    
         $this->insert = $this->db->prepare("insert  into  Employe(email,  mdp,  nom,  prenom,  idRole)values (:email, :mdp, :nom, :prenom, :role)");
         $this->connect = $this->db->prepare("select   C.email,   E.idRole,   C.mdp   from   Employe E, Compte C where E.idCompte = C.id and C.email=:email");
-        $this->select = $db->prepare("select u.id, email, nom, prenom, idRole, r.libelle as libellerole from Employe E, role r where u.idRole = r.id order by email");
-}
+        $this->select = $db->prepare("select E.id, email, nom, prenom, idRole, r.libelle as libellerole from Employe E, role r where E.idRole = r.id order by email");
+        $this->selectByAccount = $this->db->prepare("SELECT E.id, nom, prenom, idRole, idCompte FROM Employe E, Compte C WHERE C.email = :email");
+    }
 
     public function insert($email, $mdp, $role, $nom, $prenom){ // Étape 3         
         $r = true;        
@@ -33,6 +35,13 @@ class Employe{
 
     public function select(){        
         $this->select->execute();        
+        if ($this->select->errorCode()!=0){             
+            print_r($this->select->errorInfo());          
+        }        
+        return $this->select->fetchAll();    
+    }
+    public function selectByAccount($email){        
+        $this->selectByAccount->execute(array(':email'=>$email));        
         if ($this->select->errorCode()!=0){             
             print_r($this->select->errorInfo());          
         }        
