8e9911570a912dc8b06ec7f09e769e77256c448b
Valentin Mairesse <mairesse.valentin.pro@gmail.com>
2022-03-01 09:42:34 +0100

    msgs

--- public/JS/script.js
+++ public/JS/script.js
@@ -1,43 +1,43 @@
 var play = document.getElementById("play");
 var scoreText = document.getElementById("scoreText");
-scoreText.setAttribute("hidden",true);
+scoreText.setAttribute("hidden", true);
 var plateau = null;
 var interval;
 var timeOnPressed = null;
 var timeStamp = Date.now();
 
-play.addEventListener("click",function(){
+play.addEventListener("click", function() {
     plateau = new Plateau(400, 400);
     scoreText.removeAttribute("hidden");
-    nomJoueur.setAttribute("hidden",true);
-    play.setAttribute("hidden",true);
+    nomJoueur.setAttribute("hidden", true);
+    play.setAttribute("hidden", true);
     window.addEventListener('keydown', onKeyPressed);
-    play.setAttribute('disabled','disabled;')
+    play.setAttribute('disabled', 'disabled;')
     start();
 
 });
 
-function onKeyPressed(e){
+function onKeyPressed(e) {
     timeOnPressed = Date.now();
-    if(timeOnPressed - timeStamp > 100){
+    if (timeOnPressed - timeStamp > 100) {
         plateau.updatePos(e.keyCode);
         timeStamp = Date.now();
     }
 }
 
-function start(){
-   interval = setInterval(test,300);
+function start() {
+    interval = setInterval(test, 300);
 }
 
-function test(){
-    if(plateau.getGameOver() == true){
-        window.removeEventListener('keydown',onKeyPressed);
+function test() {
+    if (plateau.getGameOver() == true) {
+        window.removeEventListener('keydown', onKeyPressed);
         $("#finalScore").val(plateau.getScore());
         $("#save").removeAttr("hidden");
         plateau.stop();
         clearInterval(interval);
-    }else {
+    } else {
         plateau.moveAction();
     }
-    
+
 }
--- public/JS/scriptR.js
+++ public/JS/scriptR.js
@@ -1,67 +1,66 @@
-$(document).ready(function () {
+$(document).ready(function() {
     var id;
     modalAccueil();
     modalMessagerie();
     addDevToEquipe();
-    $(".close").click(function () {
+    $(".close").click(function() {
         $("#myModal").modal('hide');
     })
     const apiUrl = 'https://geo.api.gouv.fr/communes?codePostal=';
-    const format =  '&format=json';
+    const format = '&format=json';
 
     let zipcode = $('#codePostal');
     let city = $('#region');
-    $('#codePostal').on('blur',function(){
+    $('#codePostal').on('blur', function() {
         $(city).empty();
         let code = $(this).val();
-        let url = apiUrl+code+format;
-        fetch(url,{method: 'get'}).then(response => response.json()).then(results => {
-            if(results.length > 1){
-                $.each(results,function(key,value){
-                $(city).append('<option value="'+value.nom+'">'+value.nom+'</option>');
+        let url = apiUrl + code + format;
+        fetch(url, { method: 'get' }).then(response => response.json()).then(results => {
+            if (results.length > 1) {
+                $.each(results, function(key, value) {
+                    $(city).append('<option value="' + value.nom + '">' + value.nom + '</option>');
                 });
             }
-        }).catch(err =>{
+        }).catch(err => {
             console.log(err);
         });
     });
 
-
 });
 
 function modalAccueil() {
-    $(".deleteButton").click(function () {
+    $(".deleteButton").click(function() {
         $("#myModal").modal('show');
         id = $("#delbutton").val();
     })
 
-    $("#perform").click(function () {
+    $("#perform").click(function() {
         window.location.href = "https://s3-4223.nuage-peda.fr/index.php?page=accueil&item=1&idDel=" + id;
     })
 }
 
 function modalMessagerie() {
-    $("#newMessageButton").click(function () {
+    $("#newMessageButton").click(function() {
         $("#myModal").modal('show');
     })
 }
 
 function addDevToEquipe() {
-    $("#buttonAdd").click(function () {
-        var line = document.createElement("li");    
+    $("#buttonAdd").click(function() {
+        var line = document.createElement("li");
         var selector = document.createElement("select");
         selector.setAttribute('class', 'selectors');
         selector.setAttribute('name', 'selectors[]');
         var option;
-        $("#selectModel").children().each(function (index) {
-            if (isValueExisting(checkSelectors(), $("#selectModel").children()[index].innerText) == false) {         
+        $("#selectModel").children().each(function(index) {
+            if (isValueExisting(checkSelectors(), $("#selectModel").children()[index].innerText) == false) {
                 option = document.createElement("option");
                 option.innerText = $("#selectModel").children()[index].innerText;
                 option.value = $("#selectModel").children()[index].value;
                 selector.append(option);
             }
         })
-        if(selector.children.length > 0){
+        if (selector.children.length > 0) {
             line.append(selector);
             $("#listDev").append(line);
         }
@@ -71,7 +70,7 @@ function addDevToEquipe() {
 function checkSelectors() {
     var selectors = document.getElementsByClassName('selectors');
     var values = [];
-    $.each(selectors, function (index) {
+    $.each(selectors, function(index) {
         console.log($(this).children(':selected').text());
         if (isValueExisting(values, $(this).children(':selected').text()) == false) {
             values[index] = $(this).children(':selected').text();
@@ -82,7 +81,7 @@ function checkSelectors() {
 
 function isValueExisting(array, value) {
     var verif = false;
-    $.each(array, function (index) {
+    $.each(array, function(index) {
         if (array[index] == value) {
             verif = true;
         }
@@ -90,3 +89,10 @@ function isValueExisting(array, value) {
     return verif;
 }
 
+$(".message").click(function() {
+    console.log(this.id);
+
+    var id = this.id;
+
+    $("#" + id).show("slow");
+})
